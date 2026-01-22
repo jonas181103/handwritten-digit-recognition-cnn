0f41d942c071a45dad9b02932cf5756e2d7a692d
Simon Wameling <simon.wameling@stud.th-luebeck.de>
2026-01-22 22:06:42 +0100

    - added DocStrings - implemented with for loading the weights - switched to os.join() for filepaths

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,20 +1,36 @@
+""" Stellt die Klasse NeuralNetwork zur Verfügung. """
+
+import os
 import numpy
 import scipy
 
 # Neuronales Netz Klassen Definition
 class NeuralNetwork:
+    """
+    Diese Klasse soll ein neuronales Netzwerk repräsentieren.
+    Sie stellt Instanzmethoden zum Trainieren, Fragen, Speichern und
+    Laden einer NeuralNetwork-Instanz bereit.
+    """
 
     # Initialisierung des neuronalen Netzes
     def __init__(self, inputnodes = 784, hiddennodes = 600, outputnodes = 10, learningrate = 0.3):
+        """
+        Initialisierung der Gewichtungsmatrizen (wih und who) des neuronalen Netzes mit
+        kleinen zufälligen Werten zwischen -0.5 und 0.5 (zufällig), um die Symmetrie
+        der Eingaben zu verhindern.
+
+        :param inputnodes: Anzahl der Eingabewerte das NN-Objekt erwartet
+        :param hiddennodes: Anzahl der Nodes in der Hidden Layer
+        :param outputnodes: Anzahl der Ausgabe-Nodes
+        :param learningrate: Lernrate des NN-Objektes
+        """
         # Anzahl der Knoten in der Eingabeschicht, der versteckten Schicht und Ausgabeschicht
         self.inodes = inputnodes
         self.hnodes = hiddennodes
         self.onodes = outputnodes
-        # Initialisierung der Gewichtungsmatrizen des neuronalen Netzes (wih und who) mit kleinen zufälligen Werten zwischen -0.5 und 0.5
-        # Zufällig um die Symmetrie der Eingaben zu verhindern
-        # Ausgangspunkt für die Backpropagation
-        # die Funktion entnimmt Stichproben aus einer Normalverteilung
-        # Parameter: Mittelwert der Verteilung (0.0), Standardabweichung, Größe eines numpy-Arrays
+        # Ausgangspunkt für die Backpropagation: Die Funktion entnimmt ...
+        # Stichproben aus einer Normalverteilung
+        # Parameter: Mittelwert der Verteilung (0.0) Standardabweichung, Größe eines numpy-Arrays
         self.wih = numpy.random.normal(
             0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes)
         )
@@ -23,11 +39,18 @@ class NeuralNetwork:
         )
         # Lernrate
         self.lr = learningrate
+        # Lambda-Ausdruck, um die Aktivierungsfunktion zu speichern
         self.activation_function = lambda x: scipy.special.expit(x)
-        pass
 
     # Training des neuronalen Netzes
     def train(self, inputs_list, targets_list):
+        """
+        Trainiert das neuronale Netzwerk.
+
+        :param inputs_list: Eingabewerte mit denen trainiert wird
+        :param targets_list: eigentlich erwartete Output-Werte des Netzwerks
+        :return: None
+        """
         # inputs in ein 2D überführen
         inputs = numpy.array(inputs_list, ndmin=2).T
         targets = numpy.array(targets_list, ndmin=2).T
@@ -46,24 +69,29 @@ class NeuralNetwork:
         # und für jeden Knoten der versteckten Schicht entsprechend zusammengefasst
         hidden_errors = numpy.dot(self.who.T, output_errors)
 
-        # Aktualisierung der Gewichte zwischen den Verbindungen der versteckten Schicht und der Ausgabeschicht
-        # Genauer: Lernrate self.lr wird mit dem Rest multipliziert; numpy.dot() für Matrizenmultiplikation
+        # Aktualisierung der Gewichte zwischen den Verbindungen der ...
+        # versteckten Schicht und der Ausgabeschicht
+        # Genauer: Lernrate self.lr wird mit dem Rest multipliziert; ...
+        # numpy.dot() für Matrizenmultiplikation
         self.who += self.lr * numpy.dot(
             (output_errors * final_outputs * (1.0 - final_outputs)),
             numpy.transpose(hidden_outputs),
         )
-        # Aktualisierung der Gewichte zwischen den Verbindungen der Eingabeschicht und der versteckten Schicht
+        # Aktualisierung der Gewichte zwischen den Verbindungen der ...
+        # Eingabeschicht und der versteckten Schicht
         self.wih += self.lr * numpy.dot(
             (hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
             numpy.transpose(inputs),
         )
-        pass
 
     def query(self, inputs_list):
-        """Abfrage des neuronalen Netzes (übernimmt die Eingabe in das neuronale Netz und liefert die Ausgabe des Netzes zurück)
+        """
+        Abfrage des neuronalen Netzes (übernimmt die Eingabe in
+        das neuronale Netz und liefert die Ausgabe des Netzes zurück)
 
-        Output:
-        Liefert ein Tupel der Ausgabe zurück (Anzahl der Elemente hängt von der Anzahl der Output Layers ab)
+        :param inputs_list:
+        :return: Liefert ein Tupel der Ausgabe zurück
+        (Anzahl der Elemente hängt von der Anzahl der Output Layers ab)
         """
         # Konvertierung der Eingaben in einen 2D-Array
         inputs = numpy.array(inputs_list, ndmin=2).T
@@ -77,22 +105,35 @@ class NeuralNetwork:
         final_outputs = self.activation_function(final_inputs)
         return final_outputs
 
-    def save_weights(self, filename="weights.npz", path="models/"):
-        """Speichert die aktuellen Gewichtsmatrizen komprimiert in eine .npz Datei."""
+    def save_weights(self, filename="weights.npz", path="models"):
+        """
+        Speichert die aktuellen Gewichtsmatrizen komprimiert in eine .npz Datei.
+
+        :param filename: Name der Datei
+        :param path: Pfad der Datei
+        :return: None
+        """
         try:
-            # savez_compressed statt savez
-            numpy.savez_compressed(path + filename, wih=self.wih, who=self.who)
+            # speichert die Weights komprimiert ab
+            numpy.savez_compressed(os.path.join(path, filename), wih=self.wih, who=self.who)
             print(f"Saved as {filename}")
         except IOError as e:
             print(f"Error {e.strerror} while trying to save '{e.filename}'")
 
-    def load_weights(self, filename="weights.npz", path="models/"):
-        """Lädt die Gewichtsmatrizen aus einer .npz Datei."""
+    def load_weights(self, filename="weights.npz", path="models"):
+        """
+        Lädt die Gewichtsmatrizen aus einer .npz Datei in das NN-Objekt.
+
+        :param filename: Name der Datei
+        :param path: Pfad der Datei
+        :return: None
+        """
         try:
-            # load funktioniert für .npy und .npz automatisch
-            data = numpy.load(path + filename)
-            self.wih = data['wih']
-            self.who = data['who']
+            # öffnen des Archivs mittels vorgesehener Funktion aus numpy
+            with numpy.load(os.path.join(path, filename)) as data:
+                # Weights in die entsprechenden Variablen speichern
+                self.wih = data['wih']
+                self.who = data['who']
             print(f"File {filename} was loaded")
         except FileNotFoundError as e:
             print(f"Error {e.strerror}: File '{e.filename}' not found")
