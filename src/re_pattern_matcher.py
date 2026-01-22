058a54c807f45a9ff3b53c727b417b523d1a745e
Simon Wameling <simon.wameling@stud.th-luebeck.de>
2026-01-22 21:30:35 +0100

    DocStrings hinzugefügt

--- src/re_pattern_matcher.py
+++ src/re_pattern_matcher.py
@@ -1,8 +1,17 @@
+""" Stellt die Klasse REqual zur Verfügung """
+
 import re
 
 class REqual(str):
-    """Override str.__eq__ to match a regex pattern.
-    Source: https://discuss.python.org/t/structural-pattern-matching-should-permit-regex-string-matches/22700/9
+    """
+    Überschreibe str.__eq__ zum matchen eines regulären Ausdrucks.
+    Inspiration: https://discuss.python.org/t/structural-pattern-matching-should-permit-regex-string-matches/22700/9
     """
     def __eq__(self, pattern):
+        """
+        Vergleicht den String mit einem Pattern.
+
+        :param pattern: regulärer Ausdruck als String
+        :return: True, falls das Pattern passt
+        """
         return re.fullmatch(pattern, self)
